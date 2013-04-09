fb45022b6a8d480c25b907e3a68754b9cfa32fe8
tseale <tseale@nd.edu>
2013-04-09 17:20:42 -0400

    create thread array with train info

--- cartmanager.c
+++ cartmanager.c
@@ -11,12 +11,51 @@ Project 5 - Synchronization & Deadlocks
 #include <pthread.h>
 #include <errno.h>
 #include <time.h>
+#include <string.h>
+
+typedef struct{
+	char direction;
+	int qPos;
+	int trainID;
+}train;
+
+// function to run for each thread
+void *arrive(void *c){
+	train* cart = (train*)c; // set the data to a variable so it can be used
+}
 
 int main (int argc, char* argv[]){
-	char *str;
+	pthread_t* trains = malloc((int)strlen(argv[1])*sizeof(pthread_t)); // create an array of threads for as many trains as given
+	
+	char *str; // hold argv[1] to iterate through characters
+	int id=0; // overall trainID
+	int ncount=0, ecount=0, scount=0, wcount=0; // count for each direction
 	for(str=argv[1]; *str; str++){
+		train cart;
 		char c=*str;
-		printf("%c\n",c);
+		switch (c){
+			case 'n':
+				cart.qPos=ncount;
+				ncount++;
+				break;
+			case 'e':
+				cart.qPos=ecount;
+				ecount++;
+				break;
+			case 's':
+				cart.qPos=scount;
+				scount++;
+				break;
+			case 'w':
+				cart.qPos=wcount;
+				wcount++;
+				break;
+		}
+		cart.direction=c;
+		cart.trainID=id;
+		printf("Cart Direction: %c\tCart Queue Pos: %d\tCart ID: %d\n",cart.direction,cart.qPos,cart.trainID);
+		pthread_create(&trains[id], NULL, arrive, (void *)&cart);
+		id++;
 	}
-	pthread_t* trains = malloc(atoi(argv[1])*sizeof(pthread_t)); // create an array of threads for as many trains as given
+
 }
