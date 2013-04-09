24686e22f2112dd4cc99b49cca6acb517615a456
Ryan Alexander Shea <rshea@student02.cse.nd.edu>
2013-04-09 17:59:17 -0400

    got rid of dumb queue attempt

--- cartmanager.c
+++ cartmanager.c
@@ -15,47 +15,31 @@ Project 5 - Synchronization & Deadlocks
 
 typedef struct{
 	char direction;
-	int qPos;
 	int trainID;
 }train;
 
-// function to run for each thread
-void *arrive(void *c){
-	train* cart = (train*)c; // set the data to a variable so it can be used
+//main function for each thread
+void *thread_main(void *c){
+    train* cart = (train*)c; //set the data to a variable so it can be used
+    printf("Cart ID: %d\t Cart Direction: %c\n",cart->trainID,cart->direction);
 }
 
 int main (int argc, char* argv[]){
-	pthread_t* trains = malloc((int)strlen(argv[1])*sizeof(pthread_t)); // create an array of threads for as many trains as given
-	
+	pthread_t* trains = malloc((int)strlen(argv[1])*sizeof(pthread_t)); // create an array of threads for as many trains as given	
 	char *str; // hold argv[1] to iterate through characters
 	int id=0; // overall trainID
-	int ncount=0, ecount=0, scount=0, wcount=0; // count for each direction
 	for(str=argv[1]; *str; str++){
 		train cart;
 		char c=*str;
-		switch (c){
-			case 'n':
-				cart.qPos=ncount;
-				ncount++;
-				break;
-			case 'e':
-				cart.qPos=ecount;
-				ecount++;
-				break;
-			case 's':
-				cart.qPos=scount;
-				scount++;
-				break;
-			case 'w':
-				cart.qPos=wcount;
-				wcount++;
-				break;
-		}
 		cart.direction=c;
 		cart.trainID=id;
-		printf("Cart Direction: %c\tCart Queue Pos: %d\tCart ID: %d\n",cart.direction,cart.qPos,cart.trainID);
-		pthread_create(&trains[id], NULL, arrive, (void *)&cart);
+		//printf("Cart Direction: %c\tCart ID: %d\n",cart.direction,cart.trainID);
+		pthread_create(&trains[id], NULL, thread_main, (void *)&cart);
 		id++;
 	}
 
+    int i;
+    for(i=0;i<id;i++){pthread_join(trains[i], NULL);}
+
+    return 0;
 }
