198b9c5d8372fc54a2031cb566be64cc686f78cb
tseale <tseale@nd.edu>
2013-04-09 20:28:07 -0400

    starting to get these mutexes...

--- cartmanager.c
+++ cartmanager.c
@@ -3,11 +3,12 @@ Taylor Seale & Ryan Shea
 Operating Systems
 April 9, 2013
 
-Project 5 - Synchronization & Deadlocks
+Project 4 - Synchronization & Deadlocks
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <errno.h>
 #include <time.h>
@@ -18,38 +19,42 @@ typedef struct{
 	int trainID;
 }train;
 
-pthread_mutex_t intersection; // mutex for the train intersection
+pthread_mutex_t intersection = PTHREAD_MUTEX_INITIALIZER; // mutex for the train intersection
+pthread_cond_t queueCart = PTHREAD_COND_INITIALIZER;
+pthread_cond_t precedence = PTHREAD_COND_INITIALIZER;
 
-//main function for each thread
-void *thread_main(void *c){
-    train* cart = (train*)c; //set the data to a variable so it can be used
-    printf("Cart ID: %d\t Cart Direction: %c\n",cart->trainID,cart->direction);
-}
 
 void arrive(train c){
 
 }
 
 void cross(train c){
-
+	sleep(1);
 }
 
 void leave(train c){
-	
+
+}
+
+//main function for each thread
+void *manage_thread(void *c){
+    train* cart = (train*)c; //set the data to a variable so it can be used
+    arrive(*cart);
+    cross(*cart);
+    leave(*cart);
 }
 
 int main (int argc, char* argv[]){
 	pthread_t* trains = malloc((int)strlen(argv[1])*sizeof(pthread_t)); // create an array of threads for as many trains as given	
 	char *str; // hold argv[1] to iterate through characters
 	int id=0; // overall trainID
-	pthread_mutex_init(&intersection,NULL);
 	for(str=argv[1]; *str; str++){
 		train cart;
 		char c=*str;
 		cart.direction=c;
 		cart.trainID=id;
 		//printf("Cart Direction: %c\tCart ID: %d\n",cart.direction,cart.trainID);
-		pthread_create(&trains[id], NULL, thread_main, (void *)&cart);
+		pthread_create(&trains[id], NULL, manage_thread, (void *)&cart);
 		id++;
 	}
 
